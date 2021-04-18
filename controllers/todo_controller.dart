d383d90e04058f4ada2cb26b79368904a331e2eb
lou <dustyfingers01257@gmail.com>
2021-04-18 15:44:10 -0600

    made server return the json it is given and began writing the todo controller

---
+++ controllers/todo_controller.dart
@@ -0,0 +1,3 @@
+class Todo {
+  // functions here that handle todo fetching, creation, editing and deleting
+}
--- server.dart
+++ server.dart
@@ -1,16 +1,36 @@
 import 'dart:io';
 import 'dart:convert';
 
+import 'controllers/todo_controller.dart';
+
 Future main() async {
   var server = await HttpServer.bind(InternetAddress.loopbackIPv4, 5000);
-
   print('Server started on port ${server.port}');
 
-  await for (HttpRequest request in server) {
-    String content = await utf8.decoder.bind(request).join();
-    var data = jsonDecode(content) as Map;
-    print(content);
-    print(data);
-    await request.response.close();
+  await for (HttpRequest req in server) {
+    // req.uri.path, req.method and req.headers would be pretty useful for creating endpoints...
+
+    switch (req.uri.path) {
+      case '/':
+        try {
+          String dataAsJson = await utf8.decoder.bind(req).join();
+          var dataAsMap = jsonDecode(dataAsJson) as Map;
+          print(dataAsJson);
+          print(dataAsMap);
+          req.response.write(dataAsJson);
+          await req.response.close();
+        } catch (error) {
+          print(error);
+          req.response.write(error);
+          await req.response.close();
+        }
+        break;
+      case '/todo':
+        // fetch, create, edit and delete todo routes here
+        break;
+      default:
+        req.response.write('');
+        await req.response.close();
+    }
   }
 }
